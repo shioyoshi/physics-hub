3444c174fb7d3e59b495ed26b7978e0558eb7126
shioyoshi <12910140.stu.kaijo.ed.jp@gmail.com>
2026-04-05 15:32:44 +0900

    fix auth persistence

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,5 +1,9 @@
 import { initializeApp, getApps } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
 import { initializeFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
@@ -8,7 +12,8 @@ const firebaseConfig = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
@@ -18,6 +23,9 @@ const app =
     : getApps()[0];
 
 export const auth = getAuth(app);
+
+// 🔥 これが重要
+setPersistence(auth, browserLocalPersistence);
 
 export const db = initializeFirestore(app, {
   experimentalForceLongPolling: true,
