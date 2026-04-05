e723be9e001a4302212e6fbc7aa8bd1eb72a09ef
shioyoshi <12910140.stu.kaijo.ed.jp@gmail.com>
2026-04-05 13:29:03 +0900

    force long polling firestore

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -20,7 +20,7 @@ const app =
 export const auth = getAuth(app);
 
 export const db = initializeFirestore(app, {
-  experimentalAutoDetectLongPolling: true,
+  experimentalForceLongPolling: true,
 });
 
 export const storage = getStorage(app);
