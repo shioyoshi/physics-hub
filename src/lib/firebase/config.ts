a52c4c1cc5c472b9449f5951c47a50e18ed785f3
shioyoshi <12910140.stu.kaijo.ed.jp@gmail.com>
2026-04-05 13:22:42 +0900

    fix firestore config

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -21,7 +21,6 @@ export const auth = getAuth(app);
 
 export const db = initializeFirestore(app, {
   experimentalAutoDetectLongPolling: true,
-  useFetchStreams: false,
 });
 
 export const storage = getStorage(app);
